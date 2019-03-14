bd6e28629d828724a4aec188caa53e0e45bad749
spirit <catchspirit37@gmail.com>
2019-03-14 15:30:38 +0800

    z - Thu Mar 14 2019 15:30:38 GMT+0800 (China Standard Time)

--- task/deploySocks.sh
+++ task/deploySocks.sh
@@ -9,7 +9,7 @@ echo 'docker enabled';
 docker run --restart=always -d --name socks5 -p 1080:1080 -e PROXY_USER=hfs -e PROXY_PASSWORD=proxy@hfs serjs/go-socks5-proxy;
 echo 'docker socks container started';
 sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/g' /etc/sysctl.conf;
-echo 'net.ipv4.ip_forward changed to 0'
+echo 'net.ipv4.ip_forward changed to 1'
 systemctl restart network;
 ENDTIME=$(date +%s)
 echo 'socks5 deployed successfually :-)';
