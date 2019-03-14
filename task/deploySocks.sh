a81722d48868cf0638dc5db10c17dbf7e052585e
spirit <catchspirit37@gmail.com>
2019-03-14 14:04:23 +0800

    z - Thu Mar 14 2019 14:04:23 GMT+0800 (China Standard Time)

--- task/deploySocks.sh
+++ task/deploySocks.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo 'deployment start';
-curl -S https://get.docker.com -o get-docker.sh | bash;
+curl -S https://get.docker.com| bash;
 systemctl start docker;
 systemctl enable docker;
 docker run --restart=always -d --name socks5 -p 1080:1080 -e PROXY_USER=hfs -e PROXY_PASSWORD=proxy@hfs serjs/go-socks5-proxy;
