e7765902ca377c1129b3424e5bf22e18351f5de6
spirit <catchspirit37@gmail.com>
2019-03-14 13:52:43 +0800

    z - Thu Mar 14 2019 13:52:43 GMT+0800 (China Standard Time)

--- task/deploySocks.sh
+++ task/deploySocks.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+echo 'deploy start';
 curl -sSL https://get.docker.com -o get-docker.sh | bash;
 systemctl start docker;
 systemctl enable docker;
-docker run --restart=always -d --name socks5 -p 1080:1080 -e PROXY_USER=hfs -e PROXY_PASSWORD=proxy@hfs serjs/go-socks5-proxy
-sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/g' /etc/sysctl.conf
-systemctl restart network
+docker run --restart=always -d --name socks5 -p 1080:1080 -e PROXY_USER=hfs -e PROXY_PASSWORD=proxy@hfs serjs/go-socks5-proxy;
+sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/g' /etc/sysctl.conf;
+systemctl restart network;
+echo 'socks5 deployed successfually :-)';
