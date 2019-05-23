cc5a9dd1b5ba6a5e7723b6f15cf61aab6cf1f743
spirit <catchspirit37@gmail.com>
2019-05-23 18:08:14 +0800

    z - Thu May 23 2019 18:08:14 GMT+0800 (CST)

--- task/deployGuacd.sh
+++ task/deployGuacd.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-yum install java-1.8.0-openjdk cairo-devel libjpeg-turbo-devel libjpeg-devel libpng-devel uuid-devel libvncserver-devel openssl-devel gcc tomcat -y &&
+yum install java-1.8.0-openjdk-devel cairo-devel libjpeg-turbo-devel libjpeg-devel libpng-devel uuid-devel libvncserver-devel openssl-devel gcc tomcat -y &&
 echo 'dependency installed' &&
 curl https://raw.githubusercontent.com/spirit1453/download/master/console/java.security -o /usr/lib/jvm/java-1.8.0-openjdk-1.8.0.212.b04-0.el7_6.x86_64/jre/lib/security/java.security &&
 curl http://archive.apache.org/dist/incubator/guacamole/0.9.11-incubating/source/guacamole-server-0.9.11-incubating.tar.gz -o /opt/guacamole-server-0.9.11-incubating.tar.gz &&
