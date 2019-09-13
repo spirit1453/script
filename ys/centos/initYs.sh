3d52480e7891c221cd55484d6899494efa0eddea
catch <catchspirit37@gmail.com>
2019-09-13 10:12:48 +0800

    z - Fri Sep 13 2019 10:12:48 GMT+0800 (China Standard Time)

--- ys/centos/initYs.sh
+++ ys/centos/initYs.sh
@@ -8,6 +8,6 @@ startTime=`getTime`
 echo "Time elapsed: `expr $endTime - $startTime` s"
 yum install -y gcc-c++ make
 curl -sL https://rpm.nodesource.com/setup_12.x | sudo -E bash -
-sudo yum install nodejs
+yum install -y nodejs
 npm i spirit1453/global_script#dev_z -g
 endTime=`getTime`
