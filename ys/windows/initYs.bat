12497ca7eab7380b46824487134bbe7861059cbf
catch <catchspirit37@gmail.com>
2020-01-09 14:50:28 +0800

    z - Thu Jan 09 2020 14:50:28 GMT+0800 (China Standard Time)

--- ys/windows/initYs.bat
+++ ys/windows/initYs.bat
@@ -1,1 +1,5 @@
-powershell -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+powershell -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
+&& SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+&& choco install nodejs -y
+&& choco install git -y
+&&
