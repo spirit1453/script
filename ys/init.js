e0771a3e7a47bc65cd21773bf340e83829053da2
catch <catchspirit37@gmail.com>
2020-01-09 16:00:32 +0800

    z - Thu Jan 09 2020 16:00:32 GMT+0800 (China Standard Time)

--- ys/init.js
+++ ys/init.js
@@ -1,3 +1,4 @@
+// cation: only node standard modules are allowed in this script
 const fs = require('fs')
 const path = require('path')
 const os = require('os')
@@ -5,5 +6,8 @@ const os = require('os')
 start()
 
 async function start() {
+    const nonSystemDistDir = "D:"
+    console.log(fs.existsSync(nonSystemDistDir))
     const workingOnPath = path.resolve(os.homedir(), 'entry/code/working_on')
+    console.log(workingOnPath)
 }
